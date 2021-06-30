045e264ace7f8f89b77cd7d24062c605b19d1023
unknown <adorkoreunice2@gmail.com>
2021-06-30 13:54:18 +0000

    pics added

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 const Projects = ({ data }) => {
   console.log(data)
@@ -16,13 +17,14 @@ const Projects = ({ data }) => {
           {projects.map(project => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div>
+              <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                 <h3>{ project.frontmatter.title }</h3>
                 <p>{ project.frontmatter.stack }</p>
               </div>
             </Link>
           ))}
         </div>
-        <p>Interested to see more? Email me a {contact} for more details</p>
+        <p>Interested to see more? Email me at {contact} for more details</p>
       </div>
     </Layout>
   );
@@ -33,18 +35,25 @@ export default Projects
 
 // export page query
 export const query = graphql`
-  query ProjectsPage {
-    pro: allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
+query ProjectsPage {
+  pro: allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
     nodes {
       frontmatter {
         slug
         stack
         title
+        thumb {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
       id
     }
   }
-  cont: site{
+  cont: site {
     siteMetadata {
       contact
     }
